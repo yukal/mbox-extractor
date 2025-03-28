be71b7265272390bf4f8eaf4d940e79abee1c679
Alex Yu <alex.yu.dev@gmail.com>
2025-03-29 00:27:43 +0200

    refactor: update demo

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"log"
+	"os"
 )
 
 func main() {
-	destinationDir := "eml"
-	mboxFile := "letters.mbox"
+	file, err := os.Open("letters.mbox")
 
-	if err := ExtractTo(destinationDir, mboxFile); err != nil {
+	if err != nil {
+		log.Fatalf("error opening .mbox file: %v", err)
+	}
+
+	defer file.Close()
+
+	if err := ExtractTo("eml", file); err != nil {
 		log.Fatal(err)
 	}
 }
